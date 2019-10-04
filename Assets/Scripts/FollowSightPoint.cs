2ba3a35d5e0775d2462e55014c1cb33b62afd925
AntoineCollot <antoine.collot@live.com>
2019-10-05 00:37:38 +0200

    Initial

---
+++ Assets/Scripts/FollowSightPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSightPoint : MonoBehaviour
+{
+    RaycastLineOfSight lineOfSight;
+
+    public float smooth = 0.1f;
+    Vector3 refPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineOfSight = FindObjectOfType<RaycastLineOfSight>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, lineOfSight.hitPoint, ref refPosition, smooth);
+        transform.forward = -lineOfSight.hitNormal;
+    }
+}
---
+++ Assets/Scripts/ValidatePosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValidatePosition : MonoBehaviour
+{
+    public float maxValidAngle;
+    public Color validColor;
+    public Color invalidColor;
+    public bool positionIsValid { get; private set; }
+    Material particleMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleMaterial = GetComponent<ParticleSystemRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        positionIsValid = Vector3.Angle(Vector3.up, -transform.forward) <= maxValidAngle;
+
+        if (positionIsValid)
+            particleMaterial.color = validColor;
+        else
+            particleMaterial.color = invalidColor;
+    }
+}
